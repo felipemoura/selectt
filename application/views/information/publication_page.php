916c180ddee518ab80f57e24cd44f81c0a1b763b
Felipe Moura <mourafelp@gmail.com>
2017-11-17 11:41:21 -0200

    atualizacao publicacao
    publicacao

--- application/views/information/publication_page.php
+++ application/views/information/publication_page.php
@@ -25,6 +25,15 @@
 <div class="container animated fadeIn">
 	<div class="container center-block">
 		<h1>Publication</h1>		
+		<br>
+		<br>
+		<p align="justify"><a href="http://lattes.cnpq.br/8431784730565849" target="_blank">MELO, S. M.</a>; SOUZA, P. S. L. ; <a href="http://lattes.cnpq.br/7137178343756327" target="_blank">Souza, S. R. S.</a> . <strong>How to test your concurrent software: an approach for the selection of testing techniques.</strong> In: 2017 The 4th ACM SIGPLAN International Workshop on Software Engineering for Parallel Systems (SEPS), Vancouver. 2017. p. 42-43.</p>
+		<br>
+		<p align="justify"><a href="http://lattes.cnpq.br/8431784730565849" target="_blank">MELO, S. M.</a>; SOUZA, P. S. L. ; <a href="http://lattes.cnpq.br/7137178343756327" target="_blank">Souza, S. R. S.</a> . <strong>Towards an Empirical Study Design for Concurrent Software Testing.</strong> In: 2016 Fourth International Workshop on Software Engineering for High Performance Computing in Computational Science and Engineering (SEHPCCSE), 2016, Salt Lake. p. 49-49.</p>
+		<br>
+		<p align="justify"><a href="http://lattes.cnpq.br/8431784730565849" target="_blank">MELO, SILVANA M.</a>; <a href="http://lattes.cnpq.br/7137178343756327" target="_blank">Souza, S. R. S.</a>; SILVA, RODOLFO A. ; SOUZA, PAULO S. L. . <strong>Concurrent software testing in practice: a catalog of tools.</strong> In: the 6th International Workshop, 2015, Bergamo. Proceedings of the 6th International Workshop on Automating Test Case Design, Selection and Evaluation - A-TEST 2015, 2015. p. 31-40.</p>
+		<br>
+		<p align="justify"><a href="http://lattes.cnpq.br/8431784730565849" target="_blank">MELO, S. M.</a>; <a href="http://lattes.cnpq.br/7137178343756327" target="_blank">Souza, S. R. S.</a> . <strong>Definição de um Framework para Avaliação Sistemática de Técnicas de Teste no Contexto de Programação Concorrente.</strong> In: IV Workshop de Teses e Dissertações do CBSoft, 2014, Maceió. IV WORKSHOP DE TESES E DISSERTAÇÕES DO CBSOFT - WTDSoft 2014, 2014. v. 2. p. 79-85.</p>
 	</div>
 </div>
 
